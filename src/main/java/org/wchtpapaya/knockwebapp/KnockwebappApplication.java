f2979beae33fd2164f6324a4d7eefb2364151b9e
Che1012 <t-minibaev@mail.ru>
2021-11-04 17:24:20 +0300

    Add Spring setup

---
+++ src/main/java/org/wchtpapaya/knockwebapp/KnockwebappApplication.java
@@ -0,0 +1,13 @@
+package org.wchtpapaya.knockwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnockwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnockwebappApplication.class, args);
+	}
+
+}
