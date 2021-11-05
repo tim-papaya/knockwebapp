0f848677a5919d97ed11e7bb9495b4fe6f18d684
Che1012 <t-minibaev@mail.ru>
2021-11-05 23:38:28 +0300

    Add get request handler for android app

--- src/main/java/org/wchtpapaya/knockwebapp/Controller.java
+++ src/main/java/org/wchtpapaya/knockwebapp/Controller.java
@@ -5,15 +5,21 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
 @RestController
-@RequestMapping("/iot/knocksensor")
 public class Controller {
 
+    private static final String PORT = ":55565";
+    private static final String HTTP = "http://";
+    private static final String APP_PATH = "iot/knock";
+
     final private Logger logger = LoggerFactory.getLogger(Controller.class);
+    private boolean isKnocked = false;
 
     final private List<String> clients;
 
@@ -22,21 +28,35 @@ public class Controller {
         clients.add("192.168.1.66");
     }
 
+    @RequestMapping(value = "/iot/knocksensor", method = GET)
     @GetMapping
     public StatusMessage showStatus() {
         return new StatusMessage("OK", 200);
     }
 
+
+    @RequestMapping(value = "/iot/knocksensor", method = POST)
     @PostMapping
     public StatusMessage receiveSensorValue(@RequestBody String sensorValue) {
         String[] tokens = sensorValue.split("=");
 
         logger.info("received value from sensor: " + tokens[tokens.length - 1]);
         for (String client : clients) {
-            String result = new RestTemplate().getForObject("http://" + client + ":55565", String.class);
+            String result = new RestTemplate().getForObject(HTTP + client + PORT + APP_PATH, String.class);
         }
 
-        return new StatusMessage("OK", 202);
+        return new StatusMessage("Accepted", 202);
+    }
+
+    @RequestMapping(value = "/iot/knocklistener", method = GET)
+    @GetMapping
+    public StatusMessage checkKnock() {
+        if (isKnocked) {
+            isKnocked = false;
+            return new StatusMessage("Accepted", 202);
+        } else {
+            return new StatusMessage("No Content", 204);
+        }
     }
 
 }
